c3e5f444cf8375d8d047d31e2b77afc8a3d4a4d0
Lindsay Paterson <lrose@exou.com>
2014-08-12 16:46:36 -0400

    Added First Stylings and Basic Layout

--- LG.js
+++ LG.js
@@ -1,9 +1,10 @@
-function load_LG() {
-	if(lanugage = "English"){
-		about_LG = "About"
-		experiences_LG = "Experiences"
-		contact_LG = "Contact Me"
-		skills_LG = "Skills"
-		interests_LG = "Interests"
-	}
-}
+// function load_LG() {
+// 	if(lanugage = "English"){
+// 		about_LG = "About"
+// 		experiences_LG = "Experiences"
+// 		contact_LG = "Contact Me"
+// 		skills_LG = "Skills"
+// 		interests_LG = "Interests"
+// 	}
+// }
+//In the event this becomes useful. Else multiple alt pages, different languages, load based on language
