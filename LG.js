9a5195d299bf649ddfd500ec6ab914f1197d2147
Lindsay Paterson <lrose@exou.com>
2014-08-12 15:52:18 -0400

    Base Main Page and Language File

---
+++ LG.js
@@ -0,0 +1,9 @@
+function load_LG() {
+	if(lanugage = "English"){
+		about_LG = "About"
+		experiences_LG = "Experiences"
+		contact_LG = "Contact Me"
+		skills_LG = "Skills"
+		interests_LG = "Interests"
+	}
+}
